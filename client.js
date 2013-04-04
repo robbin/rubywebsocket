41fa6191b02df91a90c3c0aeb7ceb3900f4cd0fc
robbin <fankai@gmail.com>
2013-04-04 10:33:55 +0800

    websocket

--- client.js
+++ client.js
@@ -1,8 +1,6 @@
-<script type="text/javascript" charset="utf-8">
 if ("WebSocket" in window) {
   var ws = new WebSocket("ws://localhost:9000/ws");
   ws.onmessage = function(evt) { };
   ws.onclose = function() {  };
   ws.onopen = function() { };
 }
-</script>
